06f5ff3be149094ec5cb0c1fdcbe9d7282b974a0
frialentahnails <heathergrant202@gmail.com>
2025-08-11 11:58:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('bookingForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your booking has been submitted! Please complete payment via Cash App.');
+});
