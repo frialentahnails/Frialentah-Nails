b3b044864608fa6f980835d8b7199856e0ab9f5e
frialentahnails <heathergrant202@gmail.com>
2025-08-11 13:39:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll("button").forEach(btn => {
-        btn.addEventListener("click", () => alert(`You selected: ${btn.textContent}`));
-    });
-});
