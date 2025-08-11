b736319cc5c2693482bc89d96b2a526ed14c47ea
frialentahnails <heathergrant202@gmail.com>
2025-08-11 12:01:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('bookingForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Your booking has been submitted! Please complete payment via Cash App.');
-});
